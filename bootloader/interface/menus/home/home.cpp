08366254877e6dd8fb8a83c1ac1ecbd79feaadfd
Yaya.Cout <67095734+Yaya-Cout@users.noreply.github.com>
2022-04-09 12:21:01 +0200

    [bootloader] Add boot support

--- bootloader/interface/menus/home/home.cpp
+++ bootloader/interface/menus/home/home.cpp
@@ -1,6 +1,7 @@
 #include "home.h"
 #include <bootloader/slot.h>
 #include <bootloader/interface/menus/about/about.h>
+#include <bootloader/boot.h>
 
 Bootloader::AboutMenu * Bootloader::HomeMenu::aboutMenu() {
   static AboutMenu * aboutMenu = new AboutMenu();
@@ -13,21 +14,21 @@ Bootloader::HomeMenu::HomeMenu() : Menu(KDColorBlack, KDColorWhite, Messages::ma
 
 bool slotA_submenu() {
   if (Bootloader::Slot::isFullyValid(Bootloader::Slot::A())) {
-  
+    Bootloader::Boot::bootSlot(Bootloader::Slot::A());
   }
   return false;
 }
 
 bool slotKhi_submenu() {
   if (Bootloader::Slot::isFullyValid(Bootloader::Slot::Khi())) {
-    
+    Bootloader::Boot::bootSlot(Bootloader::Slot::Khi());
   }
   return false;
 }
 
 bool slotB_submenu() {
   if (Bootloader::Slot::isFullyValid(Bootloader::Slot::B())) {
-    
+    Bootloader::Boot::bootSlot(Bootloader::Slot::B());
   }
   return false;
 }
@@ -57,6 +58,6 @@ void Bootloader::HomeMenu::setup() {
     m_colomns[0] = Colomn(slotA_text(), Ion::Keyboard::Key::One, k_small_font, 30, false, &slotA_submenu);
     m_colomns[1] = Colomn(slotKhi_text(), Ion::Keyboard::Key::Two, k_small_font, 30, false, &slotKhi_submenu);
     m_colomns[2] = Colomn(slotB_text(), Ion::Keyboard::Key::Three, k_small_font, 30, false, &slotB_submenu);
-    m_colomns[3] = Colomn("4- installer", Ion::Keyboard::Key::Four, k_small_font, 30, false, &installer_submenu);
-    m_colomns[4] = Colomn("5- about", Ion::Keyboard::Key::Five, k_small_font, 30, false, &about_submenu);
+    m_colomns[3] = Colomn("4 - Installer", Ion::Keyboard::Key::Four, k_small_font, 30, false, &installer_submenu);
+    m_colomns[4] = Colomn("5 - About", Ion::Keyboard::Key::Five, k_small_font, 30, false, &about_submenu);
 }
